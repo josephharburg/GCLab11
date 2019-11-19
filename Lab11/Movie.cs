b574240d5c8d1dcc1c3937860fbfe942bb7f0ab7
Joseph Harburg <josephharburg@gmail.com>
2019-11-18 21:57:06 -0500

    first commit

---
+++ Lab11/Movie.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lab11
+{
+    class Movie
+    {
+        private string title;
+        private string catagory;
+        
+        //public string horror = "horror";
+        //public string animated = "animated";
+        //public string drama = "drama";
+        //public string scifi = "scifi";
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Catagory
+        {
+            get { return catagory; }
+            set { catagory = value; }
+        }
+
+        public Movie (string title, string catagory)
+        {
+            Title = title;
+            Catagory = catagory;
+        }
+
+        public static int GetNumber(List<string> list, List<Movie> listTwo)
+        {
+            int userReply = 0;
+            bool repeat = true;
+            while (repeat)
+            {
+                try
+                {
+                    userReply = int.Parse(Console.ReadLine());
+                    object test = listTwo[userReply];
+                    repeat = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Please enter a number between 1 and {list.Count}!");
+                }
+            }
+            return userReply;
+        }
+
+        public static bool Repeater()
+        {
+            bool repeat = true;
+            Console.WriteLine("Do you wish to continue?");
+            while (repeat)
+            {
+                string reply = Console.ReadLine();
+                if (string.IsNullOrEmpty(reply))
+                {
+                    Console.WriteLine("Please enter a yes or no");
+                }
+                else if (Regex.IsMatch(reply.ToLower(), @"(y)|(yes)"))
+                {
+                    repeat = false;
+                    return true;
+                }
+                else if (Regex.IsMatch(reply.ToLower(), @"(n)|(no)"))
+                {
+                    repeat = false;
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a yes or no");
+                }
+               
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> genre = new List<string>()
+            {
+            "Horror", "Animated", "Drama", "Scifi"
+            };
+
+
+            List<Movie> movieList = new List<Movie>
+            {
+                {new Movie("Interstellar", genre[3])},
+                {new Movie ("Princess Mononoke", genre[1]) },
+                {new Movie("Seven", genre[0]) },
+                {new Movie("Howls Moving Castle", genre[1]) },
+                {new Movie("The Matrix", genre[3]) },
+                {new Movie("Castaway", genre[2]) },
+                {new Movie("Star Wars", genre[3]) },
+                {new Movie("The Tree of Life",genre[2]) },
+                {new Movie("Get Out", genre[0])},
+                {new Movie("Alien", genre[0]) },
+                {new Movie("Close Encounters of the Third Kind", genre[3]) }
+            };
+
+            movieList.Sort((a,b)=> a.Title.CompareTo(b.Title));
+
+            Console.WriteLine("Hi welcome to the Movie database!\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Please select a number from the list below");
+
+                int i = 1;
+                foreach (string index in genre)
+                {
+                    Console.WriteLine($"{i}: {index}");
+                    i++;
+                }
+
+                int userNumber = Movie.GetNumber(genre, movieList);
+
+                foreach (Movie movie in movieList)
+                {
+                    if (movie.Catagory == genre[userNumber - 1])
+                    {
+                        Console.WriteLine(movie.Title);
+                    }
+                }
+                repeat = Movie.Repeater();
+            }
+            Console.WriteLine("GoodBye!");
+        }
+    }
+}
